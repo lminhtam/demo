6a785abac8a2276e77c66e2c84ac2163d0bee367
Luong Minh Tam <tam.luong-minh@internal.banvien.com>
2020-03-16 17:18:00 +0700

    Updates

---
+++ precache-manifest.af6dde9250336c584f730f4251e53b0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0530871470b0ba3ce7d3daafc3e2d691",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "815f8841546c54ea6afd",
+    "url": "/demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f4f2b47c779da44dbb0d",
+    "url": "/demo/static/js/2.cda89f18.chunk.js"
+  },
+  {
+    "revision": "28584c40993b989da1cd8728c48d3cab",
+    "url": "/demo/static/js/2.cda89f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815f8841546c54ea6afd",
+    "url": "/demo/static/js/main.8d9685a1.chunk.js"
+  },
+  {
+    "revision": "e9c5b49f1d87d0f503cc",
+    "url": "/demo/static/js/runtime-main.cbc2da9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
